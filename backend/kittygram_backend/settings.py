059c849d32a1458c9f3b9274ab7f0d42b5f7cdd6
dlozko <dlozk@ya.ru>
2023-07-28 01:13:49 +0300

    Исправление 1014

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,11 +8,11 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY', default="SECRET")
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['158.160.73.232', '127.0.0.1', 'localhost', 'dlozkkitty.ddns.net']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
--- tests/t.py
+++
@@ -1,5 +0,0 @@
-env = '158.160.73.232 127.0.0.1 localhost dlozkkitty.ddns.net'
-
-envs = env.split( ) # By default `str.split()` splits upon spaces
-
-print(envs)
