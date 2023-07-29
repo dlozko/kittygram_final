583cb07fb263657bf4b77df2de3dd90abf400673
dlozko <dlozk@ya.ru>
2023-07-29 23:56:58 +0300

    Исправление 2001

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,11 +8,14 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', default="SECRET")
+#SECRET_KEY = os.getenv('SECRET_KEY', default="SECRET")
+SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
-DEBUG = os.getenv('DEBUG')
+#DEBUG = os.getenv('DEBUG')
+DEBUG = True
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split()
+#ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(' ')
+ALLOWED_HOSTS = ['158.160.73.232', '127.0.0.1', 'localhost', 'dlozkkitty.ddns.net']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
